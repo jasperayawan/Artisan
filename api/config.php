e0c8e5323941f1e53acd55159065b143fe975887
jasperayawan <ejayawan22@gmail.com>
2026-04-14 10:50:40 +0800

    edit config

--- api/config.php
+++ api/config.php
@@ -3,11 +3,17 @@ declare(strict_types=1);
 
 function artisan_db(): PDO
 {
-    $host = getenv('DB_HOST') ?: '127.0.0.1';
+    // $host = getenv('DB_HOST') ?: '127.0.0.1';
+    // $port = getenv('DB_PORT') ?: '3306';
+    // $db   = getenv('DB_NAME') ?: 'artisan_db';
+    // $user = getenv('DB_USER') ?: 'root';
+    // $pass = getenv('DB_PASS') ?: '';
+
+    $host = getenv('DB_HOST') ?: 'sql306.infinityfree.com';
     $port = getenv('DB_PORT') ?: '3306';
     $db   = getenv('DB_NAME') ?: 'artisan_db';
-    $user = getenv('DB_USER') ?: 'root';
-    $pass = getenv('DB_PASS') ?: '';
+    $user = getenv('DB_USER') ?: 'if0_41656556';
+    $pass = getenv('DB_PASS') ?: '5m7QDSt33ozsXhU';
 
     $dsn = "mysql:host={$host};port={$port};dbname={$db};charset=utf8mb4";
 
